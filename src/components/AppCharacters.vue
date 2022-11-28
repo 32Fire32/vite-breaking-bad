115c5119cc61cf3509cf20681fc45b51f261a07a
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-28 23:37:46 +0100

    implementa la selezione della categoria

--- src/components/AppCharacters.vue
+++ src/components/AppCharacters.vue
@@ -10,30 +10,68 @@ export default {
     AppFound,
     AppLoading,
   },
+
   data() {
     return {
       characters: [],
-      cat: "",
+      filterCharacters: [],
+      cat: "Select Category",
       hiding: true,
     };
   },
+  // computed: {
+  //   filter() {
+  //     this.characters.forEach((element, i) => {
+  //       if (this.cat === "Breaking Bad") {
+  //         if (!element.category.includes("Breaking Bad")) {
+  //           this.characters.splice(i, 1);
+  //         }
+  //       } else if (this.cat === "Better Call Saul") {
+  //         if (!element.category.includes("Better Call Saul")) {
+  //           this.characters.splice(i, 1);
+  //         }
+  //       } else if (this.cat === "Select Category") {
+  //         if (element.category.includes("Select Category")) {
+  //           this.characters.splice(i, 1);
+  //         }
+  //       }
+  //     });
+  //   },
+  // },
+  methods: {},
+
   created() {
     this.hiding = false;
     axios
       .get("https://www.breakingbadapi.com/api/characters")
       .then((response) => {
-        console.log(response.data);
+        // console.log(response.data);
         this.characters = response.data;
         this.hiding = true;
       });
   },
-  // updated() {
-  //   if (this.cat === "Breaking Bad") {
-  //     this.hiding = true;
-  //     console.log(this.hiding);
-  //     // console.log("la lunghezza dei characters è" + this.characters.length);
-  //   }
-  // },
+  updated() {
+    if (this.cat === "Select Category") {
+      axios
+        .get("https://www.breakingbadapi.com/api/characters")
+        .then((response) => {
+          this.characters = response.data;
+        });
+    }
+    this.characters.forEach((element, i) => {
+      if (this.cat === "Breaking Bad") {
+        if (!element.category.includes("Breaking Bad")) {
+          this.characters.splice(i, 1);
+          this.filterCharacters.push(i);
+        }
+      } else if (this.cat === "Better Call Saul") {
+        if (!element.category.includes("Better Call Saul")) {
+          this.characters.splice(i, 1);
+          this.filterCharacters.push(i);
+        }
+      }
+    });
+  },
 };
 </script>
 
@@ -56,7 +94,10 @@ export default {
     </form>
     <AppFound :info="characters" />
     <div class="row">
-      <AppCharacterCard v-for="character in characters" :info="character" />
+      <AppCharacterCard
+        v-for="(character, index) in characters"
+        :info="character"
+      />
     </div>
   </section>
 </template>
--- src/components/AppLoading.vue
+++ src/components/AppLoading.vue
@@ -5,9 +5,39 @@ export default {
 </script>
 
 <template>
-  <div class="loading">
-    <h1>IS LOADING</h1>
+  <div class="row">
+    <div class="col-xs-12">
+      <h3>LOADING...</h3>
+    </div>
+    <div class="col-xs-12">
+      <i class="ex-11-icon fas fa-circle-notch"></i>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.ex-11-icon:hover {
+  animation-name: spin-inverse;
+  animation-duration: 2s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+}
+
+@keyframes spin-inverse {
+  0% {
+    transform: rotate(0deg);
+  }
+  25% {
+    transform: rotate(90deg);
+  }
+  50% {
+    transform: rotate(180deg);
+  }
+  75% {
+    transform: rotate(270deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
+</style>
